7e018c86e2c270993ca9df5d435c292c60750b45
aerjayc <aerjay.castaneda@gmail.com>
2020-12-16 03:16:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# To do:
+- access MangaUpdates' API
+- get top-N latest / 1st page
+  - get metadata of each
+  - do some processing to get something useful
+    - sort results by popularity
+    - get dropped manga, sort by popularity
+- get top-N results sorted by some other measure + filter using tags
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+# TODO: Error Handling
+
+def get_archived_releases(page=1, perpage=5):
+    url = 'https://www.mangaupdates.com/releases.html'
+
+    params = {'act': 'archive', 'page': page, 'perpage': perpage}
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    main_content = soup.find(id='main_content')
+    rows = main_content.select_one('div.p-2.pt-2.pb-2.text > div.row.no-gutters')
+    
+    # TODO: Use generators instead for better performance
+    table_headers = rows.select('div.releasestitle')
+    table_entries = rows.select('div.row.no-gutters > div.text')
+
+    rows = []
+    num_cols = len(table_headers)
+    for i, entry in enumerate(table_entries):
+        row_index = i // num_cols
+        col_index = i % num_cols
+        header = table_headers[col_index].text
+
+        if col_index == 0:      # if new row, create new `rows` element
+            rows.append({header: entry.text})
+        else:
+            rows[row_index][header] = entry.text
+
+    if len(rows[-1]) < num_cols:    # remove extraneous last row
+        del rows[-1]
+
+    return rows
